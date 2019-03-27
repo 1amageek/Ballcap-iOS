d4b2422811a14d4b7ff499cf0ebda076a97e1244
1amageek <tmy0x3@icloud.com>
2019-03-27 17:22:10 +0900

    Initial Commit

---
+++ Ballcap/Ballcap.h
@@ -0,0 +1,19 @@
+//
+//  Ballcap.h
+//  Ballcap
+//
+//  Created by 1amageek on 2019/03/27.
+//  Copyright © 2019 Stamp Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ballcap.
+FOUNDATION_EXPORT double BallcapVersionNumber;
+
+//! Project version string for Ballcap.
+FOUNDATION_EXPORT const unsigned char BallcapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ballcap/PublicHeader.h>
+
+
