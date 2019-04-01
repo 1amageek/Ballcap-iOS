1a2f0a345c4ea0962a08007a83f843331bfc2273
1amageek <tmy0x3@icloud.com>
2019-04-01 18:31:45 +0900

    add TestHelper to test

--- BallcapTests/TestHelper.swift
+++ BallcapTests/TestHelper.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2019 Stamp Inc. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import FirebaseFirestore
+import FirebaseStorage
+
+func assertRoundTrip<X: Equatable & Codable>(model: X, encoded: [String: Any]) -> Void {
+    let enc = assertEncodes(model, encoded: encoded)
+    assertDecodes(enc, encoded: model)
+}
+
+func assertEncodes<X: Equatable & Codable>(_ model: X, encoded: [String: Any]) -> [String: Any] {
+    do {
+        let enc = try Firestore.Encoder().encode(model)
+        XCTAssertEqual(enc as NSDictionary, encoded as NSDictionary)
+        return enc
+    } catch {
+        XCTFail("Failed to encode \(X.self): error: \(error)")
+    }
+    return ["": -1]
+}
+
+func assertDecodes<X: Equatable & Codable>(_ model: [String: Any], encoded: X) -> Void {
+    do {
+        let decoded = try Firestore.Decoder().decode(X.self, from: model)
+        print("!!!!", decoded)
+        XCTAssertEqual(decoded, encoded)
+    } catch {
+        XCTFail("Failed to decode \(X.self): \(error)")
+    }
+}
+
+func assertDecodingThrows<X: Equatable & Codable>(_ model: [String: Any], encoded: X) -> Void {
+    do {
+        _ = try Firestore.Decoder().decode(X.self, from: model)
+    } catch {
+        return
+    }
+    XCTFail("Failed to throw")
+}
