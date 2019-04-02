568c4e2e72a807bac9b0e3cdc2a3b3cd2e7091f1
1amageek <tmy0x3@icloud.com>
2019-04-02 19:30:21 +0900

    add IncrementableNumber

--- Ballcap/IncrementableNumber.swift
+++ Ballcap/IncrementableNumber.swift
@@ -1,5 +1,5 @@
 //
-//  Incremental.swift
+//  IncrementableNumber.swift
 //  Ballcap
 //
 //  Created by 1amageek on 2019/04/02.
@@ -8,7 +8,30 @@
 
 import FirebaseFirestore
 
-public enum Incremental: Codable {
+public enum IncrementableInt: Codable {
+    case increment(Int64)
+    case value(Int64)
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(Int64.self)
+        self = .value(value)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch (self) {
+        case .increment(let value):
+            try container.encode(FieldValue.increment(value))
+            break
+        case .value(let value):
+            try container.encode(value)
+            break
+        }
+    }
+}
+
+public enum IncrementableDouble: Codable {
     case increment(Double)
     case value(Double)
 
