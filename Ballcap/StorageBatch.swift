7686fd38a537676ca35354242d72a68720271dc0
1amageek <tmy0x3@icloud.com>
2019-04-11 15:44:01 +0900

    add StorageBatch

--- Ballcap/StorageBatch.swift
+++ Ballcap/StorageBatch.swift
@@ -6,4 +6,78 @@
 //  Copyright © 2019 Stamp Inc. All rights reserved.
 //
 
-import Foundation
+import FirebaseStorage
+
+public final class StorageBatch {
+
+    enum BatchType {
+        case save
+        case delete
+    }
+
+    private let _queue: DispatchQueue = DispatchQueue(label: "StorageManager.queue")
+
+    private let _group: DispatchGroup = DispatchGroup()
+
+    private var _isCommitted: Bool = false
+
+    private var _storage: [(type: BatchType, file: File)] = []
+
+    var timeout: Int = 10 // Default 10s
+
+    public init() { }
+
+    @discardableResult
+    public func save(file: File) -> Self {
+        if _isCommitted {
+            fatalError("Batch is already committed")
+        }
+        self._storage.append((.save, file))
+        return self
+    }
+
+    @discardableResult
+    public func delete(file: File) -> Self {
+        if _isCommitted {
+            fatalError("Batch is already committed")
+        }
+        self._storage.append((.delete, file))
+        return self
+    }
+
+    public func commit(_ completion: ((Error?) -> Void)? = nil) {
+        if _isCommitted {
+            fatalError("Batch is already committed")
+        }
+        if self._storage.isEmpty {
+            completion?(nil)
+            return
+        }
+        for (_, stock) in self._storage.enumerated() {
+            switch stock.type {
+            case .save:
+                self._group.enter()
+                stock.file.save { [weak self] (metadata, error) in
+                    self?._group.leave()
+                }
+            case .delete:
+                self._group.enter()
+                stock.file.delete { [weak self] (error) in
+                    self?._group.leave()
+                }
+            }
+        }
+        self._queue.async {
+            switch self._group.wait(timeout: .now() + .seconds(self.timeout)) {
+            case .success:
+                DispatchQueue.main.async {
+                    completion?(nil)
+                }
+            case .timedOut:
+                DispatchQueue.main.async {
+                    completion?(DocumentError.timeout)
+                }
+            }
+        }
+    }
+}
