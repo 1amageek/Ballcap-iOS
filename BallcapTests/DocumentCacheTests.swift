d238bfa15f78ce0960de172a6fff7d3de332cbd0
1amageek <tmy0x3@icloud.com>
2019-05-13 17:43:09 +0900

    Added cache tests

--- BallcapTests/DocumentCacheTests.swift
+++ BallcapTests/DocumentCacheTests.swift
@@ -20,6 +20,94 @@ class DocumentCacheTests: XCTestCase {
 
     // TODO: DocumentCache tests
 
+    func testDocumentCache() {
+        let exp: XCTestExpectation = XCTestExpectation(description: "")
+        struct Model: Codable, Modelable, Equatable {
+            var s: String?
+        }
+        let d: Document<Model> = Document(id: "a")
+        d.data?.s = "s"
+        d.save { _ in
+            let d: Document<Model> = Document(id: "a")
+            XCTAssertEqual(d.cache!.s!, "s")
+            d.delete(completion: { _ in
+                XCTAssertNil(d.cache)
+                exp.fulfill()
+            })
+        }
+        self.wait(for: [exp], timeout: 30)
+    }
+
+    func testDocumentCacheFromSnapshot() {
+        let exp: XCTestExpectation = XCTestExpectation(description: "")
+        struct Model: Codable, Modelable, Equatable {
+            var s: String?
+        }
+        let d: Document<Model> = Document(id: "a")
+        d.data?.s = "s"
+        d.save { _ in
+            DocumentCache.shared.clear()
+            XCTAssertNil(d.cache)
+            Document<Model>.get(id: "a", completion: { (doc, _) in
+                XCTAssertEqual(doc!.cache!.s!, "s")
+                let d: Document<Model> = Document(id: "a")
+                XCTAssertEqual(d.cache!.s!, "s")
+                d.delete(completion: { _ in
+                    XCTAssertNil(d.cache)
+                    exp.fulfill()
+                })
+            })
+        }
+        self.wait(for: [exp], timeout: 30)
+    }
+
+    func testObjectCache() {
+        let exp: XCTestExpectation = XCTestExpectation(description: "")
+        class Obj: Object, DataRepresentable, DataCacheable {
+            struct Model: Modelable & Codable {
+                var s: String?
+            }
+            var data: Model?
+        }
+        let d: Obj = Obj(id: "a")
+        d.data?.s = "s"
+        d.save { _ in
+            let d: Obj = Obj(id: "a")
+            XCTAssertEqual(d.cache!.s!, "s")
+            d.delete(completion: { _ in
+                XCTAssertNil(d.cache)
+                exp.fulfill()
+            })
+        }
+        self.wait(for: [exp], timeout: 30)
+    }
+
+    func testObjectCacheFromSnapshot() {
+        let exp: XCTestExpectation = XCTestExpectation(description: "")
+        class Obj: Object, DataRepresentable, DataCacheable {
+            struct Model: Modelable & Codable {
+                var s: String?
+            }
+            var data: Model?
+        }
+        let d: Obj = Obj(id: "a")
+        d.data?.s = "s"
+        d.save { _ in
+            DocumentCache.shared.clear()
+            XCTAssertNil(d.cache)
+            Obj.get(id: "a", completion: { (doc, _) in
+                XCTAssertEqual(doc!.cache!.s!, "s")
+                let d: Obj = Obj(id: "a")
+                XCTAssertEqual(d.cache!.s!, "s")
+                d.delete(completion: { _ in
+                    XCTAssertNil(d.cache)
+                    exp.fulfill()
+                })
+            })
+        }
+        self.wait(for: [exp], timeout: 30)
+    }
+
     func testDocumentCacheSetGet() {
         struct Model: Modelable, Codable, Equatable {
 
